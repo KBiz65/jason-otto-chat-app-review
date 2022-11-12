fa8fc5acf02763737919b243aaeaadca13e368bd
Jason Otter <jasonotter17@gmail.com>
2022-11-12 10:26:36 -0800

    add router and logging

---
+++ server/router.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+// GET all messages
+router.get("/messages", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "get all messages" });
+});
+
+// POST new message
+router.post("/messages", (req, res) => {
+  res.statusCode = 201;
+  res.json({ message: "create new message" });
+});
+
+// GET all users
+router.get("/users", (req, res) => {});
+
+// GET user by ???
+router.get("/users/:id", (req, res) => {});
+
+// POST new user
+router.post("/users", (req, res) => {});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-require("dotenv").config();
 const path = require("path");
+require("dotenv").config();
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
+const router = require("./router");
+
+app.use(morgan("dev")); // logging
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use("/", express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  console.log("we are NOT in production");
+app.use("/", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "hello from Chat App" });
 });
+
+app.use("/api", router);
 
 module.exports = app;
