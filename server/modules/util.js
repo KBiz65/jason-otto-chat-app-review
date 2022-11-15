6876187d488139e822fca79b94ca71d647f227e0
Jason Otter <jasonotter17@gmail.com>
2022-11-14 21:03:08 -0800

    add post-message handler

--- server/modules/util.js
+++ server/modules/util.js
@@ -27,7 +27,21 @@ module.exports.validateLoginData = (req, res, next) => {
     res.json({
       message: "400 | Bad Request",
       detail:
-        "One or more required parameters were not provided by the request",
+        "One or more required parameters were not provided or properly formatted by the request.",
     });
   }
+};
+
+module.exports.validateMessage = (req, res, next) => {
+  const { author_id, text_content } = req.body;
+  if (author_id && text_content) {
+    next();
+    return;
+  }
+  res.statusCode = 400;
+  res.json({
+    message: "400 | Bad Request",
+    detail:
+      "One or more required parameters were not provided or properly formatted by the request.",
+  });
 };
--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { client } = require("./modules/db");
+const { validateMessage } = require("./modules/util");
 
 // GET all messages
 router.get("/messages", async (req, res) => {
@@ -14,35 +15,32 @@ router.get("/messages", async (req, res) => {
     res.statusCode = 200;
     res.json({ message: "200 | OK", data: queryRes.rows });
   } catch (err) {
-    console.log(err);
     res.statusCode = 500;
     res.json({ message: "500 | Internal Server Error" });
   }
 });
 
 // POST new message
-router.post("/messages", (req, res) => {
-  res.statusCode = 201;
-  res.json({ message: "create new message" });
-
-  // todo: build the query
+router.post("/messages", validateMessage, async (req, res) => {
+  const { author_id, text_content } = req.body;
+  const timestamp = new Date();
   const query = {
     name: "new-message",
-    text: "INSERT INTO messages ",
+    text: "INSERT INTO messages (author_id, text_content, created_on) VALUES ($1, $2, $3)",
+    values: [author_id, text_content, timestamp],
   };
 
-  // todo: run the query
-  // todo: handle the response?
+  try {
+    await client.query(query);
+    res.statusCode = 201;
+    res.json({ message: "201 | Created" });
+  } catch (err) {
+    res.statusCode = 500;
+    res.json({ message: "500 | Internal Server Error" });
+  }
 });
 
-// GET all users
-router.get("/users", (req, res) => {
-  // todo: build the query
-  // todo: run the query
-  // todo: return the results as json
-});
-
-// GET user by ???
+// GET user by ID
 router.get("/users/:id", (req, res) => {
   // todo: build the query with the given id
   // todo: run the query
