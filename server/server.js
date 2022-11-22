4c01c16254fee82c4a2c8cc634c167e4351d1bea
Jason Otter <jasonotter17@gmail.com>
2022-11-21 22:21:24 -0800

    fix routing

--- server/server.js
+++ server/server.js
@@ -15,17 +15,12 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.statusCode = 200;
-//   res.send();
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "../client/build")));
+}
+
 app.use("/api", router);
 app.post("/signin", validateLoginData, signIn);
 app.post("/signout", signOut);
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "../client/build")));
-  a;
-}
 
 module.exports.server = createServer(app);
